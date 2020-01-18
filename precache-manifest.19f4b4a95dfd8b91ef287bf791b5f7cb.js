b1138d040bece656094ff133f3fd122b54a18c73
vesnaivanovic <ivanovic.vesna7@gmail.com>
2020-01-18 22:05:22 +0100

    Updates

---
+++ precache-manifest.19f4b4a95dfd8b91ef287bf791b5f7cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e53c08c8983a4d8f58a97afc0a7d13b6",
+    "url": "/react-todos-app/index.html"
+  },
+  {
+    "revision": "0210401c270ed9445e74",
+    "url": "/react-todos-app/static/css/main.42b394ae.chunk.css"
+  },
+  {
+    "revision": "c8c52bfae53c30c2228f",
+    "url": "/react-todos-app/static/js/2.0b52eac1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todos-app/static/js/2.0b52eac1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0210401c270ed9445e74",
+    "url": "/react-todos-app/static/js/main.0390f2a2.chunk.js"
+  },
+  {
+    "revision": "613474c0550757367896",
+    "url": "/react-todos-app/static/js/runtime-main.1167c821.js"
+  }
+]);
